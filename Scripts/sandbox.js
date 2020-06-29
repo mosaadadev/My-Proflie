654db6beb47a636896fad8c600291aabba00714f
iammosa <ramosamosa@gmail.com>
2020-06-29 13:13:51 +0200

    done

--- Scripts/sandbox.js
+++ Scripts/sandbox.js
@@ -6,3 +6,15 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+
+// this block of code makes the font weight bigger when you on a that page. ----------------------------------------------------------------------
+var header = document.getElementsByClassName("menus");
+var btns = document.getElementsByClassName("home");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active"; 
+  });
+}
